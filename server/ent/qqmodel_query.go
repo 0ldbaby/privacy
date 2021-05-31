5bdd4eaad6169319b88ff17402bb8126d845c2fa
wuqi <53676007+wq22304@users.noreply.github.com>
2021-05-31 16:04:58 +0800

    Update qqmodel_query.go

--- server/ent/qqmodel_query.go
+++ server/ent/qqmodel_query.go
@@ -11,8 +11,8 @@ import (
 	"github.com/facebook/ent/dialect/sql"
 	"github.com/facebook/ent/dialect/sql/sqlgraph"
 	"github.com/facebook/ent/schema/field"
-	"github.com/kallydev/privacy/ent/predicate"
-	"github.com/kallydev/privacy/ent/qqmodel"
+	"github.com/wq22304/privacy/ent/predicate"
+	"github.com/wq22304/privacy/ent/qqmodel"
 )
 
 // QQModelQuery is the builder for querying QQModel entities.
